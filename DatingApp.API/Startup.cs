79c11aa49e1b3be9a79beb4befb790691be5f226
sergiuradu5 <sergiuradu5@gmail.com>
2020-06-13 16:14:51 +0200

    Login&Register, UserExists, Token

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -12,7 +12,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API
 {
@@ -31,7 +33,19 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
-          services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options=> {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                         ValidateIssuerSigningKey= true,
+                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                         .GetBytes(Configuration.GetSection("AppSettings:Token").Value )),
+                         ValidateIssuer = false,
+                         ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +58,7 @@ namespace DatingApp.API
 
             //app.UseHttpsRedirection();
             app.UseCors(x=> x.AllowAnyOrigin().AllowAnyOrigin().AllowAnyHeader());
+            app.UseAuthentication();
             app.UseMvc();
             app.UseRouting();
 
